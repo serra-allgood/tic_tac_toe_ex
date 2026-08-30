70e7516d7a1617f0f8e9ec529310073c138016ad
Serra Allgood <serra@allgood.dev>
2026-08-30 06:18:10 -0400

    Create a basic layout for starting a new game
    Signed-off-by: Serra Allgood <serra@allgood.dev>

--- lib/tic_tac_toe_ex_web/components/layouts.ex
+++ lib/tic_tac_toe_ex_web/components/layouts.ex
@@ -36,9 +36,65 @@ defmodule TicTacToeExWeb.Layouts do
 
   def app(assigns) do
     ~H"""
+    <nav class="navbar" role="navigation" aria-label="main navigation">
+    <div class="navbar-brand">
+    	<a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+    		<span aria-hidden="true"></span>
+    		<span aria-hidden="true"></span>
+    		<span aria-hidden="true"></span>
+    		<span aria-hidden="true"></span>
+    	</a>
+    </div>
 
-    <main class="px-4 py-20 sm:px-6 lg:px-8">
-      <div class="mx-auto max-w-2xl space-y-4">
+    <div id="navbarBasicExample" class="navbar-menu">
+    	<div class="navbar-start">
+    		<a class="navbar-item">
+    			Home
+    		</a>
+    		<a class="navbar-item">
+    			Documentation
+    		</a>
+
+    <div class="navbar-item has-dropdown is-hoverable">
+    	<a class="navbar-link">
+    		More
+    	</a>
+
+    	<div class="navbar-dropdown">
+    		<a class="navbar-item">
+    			About
+    		</a>
+    		<a class="navbar-item is-selected">
+    			Jobs
+    		</a>
+    		<a class="navbar-item">
+    			Contact
+    		</a>
+    		<hr class="navbar-divider">
+    		<a class="navbar-item">
+    			Report an issue
+    		</a>
+    	</div>
+    </div>
+    </div>
+
+    <div class="navbar-end">
+    <div class="navbar-item">
+    	<div class="buttons">
+    		<a class="button is-primary">
+    			<strong>Sign up</strong>
+    		</a>
+    		<a class="button is-light">
+    			Log in
+    		</a>
+    	</div>
+    </div>
+    </div>
+    </div>
+    </nav>
+
+    <main class="section">
+      <div class="container">
         {render_slot(@inner_block)}
       </div>
     </main>
