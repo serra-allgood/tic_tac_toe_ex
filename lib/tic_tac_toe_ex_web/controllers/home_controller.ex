f5b9d792c266bc8bb01a5a6b44f612420d883a76
Serra Allgood <serra@allgood.dev>
2026-08-30 21:34:26 -0400

    Minor refactor of home_html/index
    Signed-off-by: Serra Allgood <serra@allgood.dev>

--- lib/tic_tac_toe_ex_web/controllers/home_controller.ex
+++ lib/tic_tac_toe_ex_web/controllers/home_controller.ex
@@ -2,6 +2,11 @@ defmodule TicTacToeExWeb.HomeController do
   use TicTacToeExWeb, :controller
 
   def index(conn, _params) do
-    render(conn, :index)
+    button_config = [
+      [visibility: "public", color: "is-primary"],
+      [visibility: "private", color: "is-link"]
+    ]
+
+    render(conn, :index, button_config: button_config)
   end
 end
